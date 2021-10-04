e625caacf3ab7ac798702a1dee22a200eda7a9ea
527m <Q4nA7qYv569gys9ATpFu>
2021-10-04 18:40:54 +0600

    task 10 end

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import preloader from './preloader.gif'
+import {AppStoreType} from "./bll/store";
+import {InitStateType, loadingOffAC, loadingOnAC} from "./bll/loadingReducer";
+import a from './HW10.module.css'
+
+// type LoadingType = {
+//     loading: boolean
+// }
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
+    const loadingOff = () => dispatch(loadingOffAC())
+    console.log(loading)
+
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingOnAC())
+        setTimeout(loadingOff, 2000)
         console.log('loading...')
     };
 
     return (
-        <div>
+        <div className={a.main}>
             <hr/>
             homeworks 10
 
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div>
+                        <img src={preloader}/>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,36 @@
-const initState = {
-
+export type InitStateType = {
+    isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const initState = {
+    isLoading: false
+}
+
+export const loadingReducer = (state = initState, action: LoadingACType): InitStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'loadingOn': {
+            return {...state, isLoading: true}
+        }
+
+        case 'loadingOff': {
+            return {...state, isLoading: false}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingOnAC = () => {
+    return {
+        type: "loadingOn"
+    } as const
+} // fix any
+
+export const loadingOffAC = () => {
+    return {
+        type: "loadingOff"
+    } as const
+} // fix any
+
+
+
+type LoadingACType = ReturnType<typeof loadingOnAC> | ReturnType<typeof loadingOffAC>
